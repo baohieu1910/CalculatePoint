44463adb4e2086aba083447ed40906646eb0195b
Bảo Hiếu <l.b.hieu.vmp@gmail.com>
2024-02-11 22:46:19 +0700

    Initial Commit

---
+++ CalculatePoint/CalculatePointApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatePointApp.swift
+//  CalculatePoint
+//
+//  Created by Hieu Le on 2/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatePointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
